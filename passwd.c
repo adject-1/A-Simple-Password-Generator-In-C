d8adcf07eda7af162ed802da06f2be02e9e52059
adject-1 <omkarkharage99@gmail.com>
2026-03-21 14:41:39 +0530

    seperated header and c

---
+++ passwd.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include "passwd.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define MIN 33
+#define MAX 126
+
+int *takeInput(){
+    int strSize;
+    printf("Enter the length of your password: ");
+    scanf("%d", &strSize);
+    return &strSize;
+}
+
+void generatePassword(int *size){
+
+    char* passwd = malloc(*size * sizeof(char));
+    if (passwd == NULL) {
+        printf("Memory allocation failed\n");
+        return;
+    }
+
+    srand(time(NULL));
+
+    int index = 0;
+    for (int i = 0; i < *size ; i++) {
+        int code = rand() % (MAX + 1 - MIN) + MIN;
+        scanf("%c", &passwd[index++]);
+    }
+
+    for (int i = 0; i< *size; i++) {
+        printf("%c", passwd[i]);
+    }
+
+    printf("\n");
+}
